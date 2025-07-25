6c5f3f2b2ef09d4aa8c2f1ad3cb8d669d8e658d6
Maximilian Hart <38765373+MaximilianHart@users.noreply.github.com>
2025-07-25 13:55:15 -0500

    feat: add mokaam api call for historical market data

---
+++ src/mokaam_call.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import sqlite3
+
+api_url = "https://mokaam.dk/API/market/all?regionid=10000002"
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    data = response.json()
+    output_json = "../data/mokaam.json"
+    with open(output_json, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+    print(f"Data has been written to {output_json}")
+else:
+    print(f"Error: {response.status_code}")
