4534a7f45fa9cccb349ff87725f9d2dbdb93ae8e
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-26 10:31:27 +0900

    Updated mokaam_call() to return opt<response, error>

--- src/mokaam_call.py
+++ src/mokaam_call.py
@@ -5,18 +5,43 @@ import json
 import sqlite3
 from preferences import get_preference
 
-region_id = get_preference("region_id")
-api_url = f"https://mokaam.dk/API/market/all?regionid={region_id}"
+class Response():
+    class Response():
+        def __init__(self):
+            self.response == None
+            self.error == None
 
-print(f"Pulling Mokaam API for region {region_id}")
+        def get_val(self):
+            if self.error != None:
+                return self.error
+            if self.response != None:
+                return self.response
+            # Neither an error or a response, must be handlded
+            raise Exception("InvalidResponse")
 
-response = requests.get(api_url)
 
-if response.status_code == 200:
-    data = response.json()
-    output_json = "../data/mokaam.json"
-    with open(output_json, "w") as outfile:
-        json.dump(data, outfile, indent=4)
-    print(f"Data has been written to {output_json}")
-else:
-    print(f"Error: {response.status_code}")
+def mokaam_call() -> Response():
+    region_id = get_preference("region_id")
+    api_url = f"https://mokaam.dk/API/market/all?regionid={region_id}"
+    res = Response()
+    print(f"Pulling Mokaam API for region {region_id}")
+
+    response = requests.get(api_url)
+    print("Received data from api")
+
+    if response.status_code == 200:
+        data = response.json()
+        res.response = data
+        output_json = "./data/mokaam.json"
+        with open(output_json, "w") as outfile:
+            json.dump(data, outfile, indent=4)
+        print(f"Data has been written to {output_json}")
+    else:
+        res.error = respionse.status_code
+        print(f"Error: {response.status_code}")
+
+    return res
+
+if __name__ == "__main__":
+    res = mokaam_call()
+    print(res)
