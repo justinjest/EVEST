505f47cf5c5c1a417b025e79a69cfb6432eef17e
Maximilian Hart <38765373+MaximilianHart@users.noreply.github.com>
2025-07-25 22:36:28 -0500

    fix: redundant logic in main.py for preference loading

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import os
 from preferences import save_preferences, load_preferences
 from discordInit import bot_init
+
 if not os.path.exists("./data"):
     preferences = {
         "region_id": "10000002",
@@ -16,14 +17,11 @@ if not os.path.exists("./data"):
     }
     save_preferences("../data/preferences.ini", preferences)
 else:
-    if os.path.exists("./data") == False:
-        os.mkdir("./data")
-        save_preferences("./data/preferences.ini", preferences)
+    load_preferences("../data/preferences.ini")
     print("Preferences loaded.")
 
 
 def __main__():
-
     bot_init()
     print("Hello, world")
 
