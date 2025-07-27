4c93206975c19e4c134d416a89d6ba197cdf5153
Maximilian Hart <38765373+MaximilianHart@users.noreply.github.com>
2025-07-27 18:42:17 -0500

    style: clear delineations of buy & sell lists

--- src/buy_sell.py
+++ src/buy_sell.py
@@ -34,9 +34,8 @@ def process_data_to_array(vals):
 
 def flag_create():
     sales_tax = float(get_preference("sales_tax"))
-    buy_fee   = float(get_preference("buy_broker_fee"))
-    sell_fee  = float(get_preference("sell_broker_fee"))
-
+    buy_fee = float(get_preference("buy_broker_fee"))
+    sell_fee = float(get_preference("sell_broker_fee"))
 
     item_ids = process_data_to_array(get_live_data())
     buy = []
@@ -53,36 +52,35 @@ def flag_create():
         buy_avg = live["buy_weighted_average"]
         sell_avg = live["sell_weighted_average"]
         buy_flag = (
-            float(buy_avg * (1.0 + buy_fee)) < avg_price - std_dev and
-            spread > avg_spread and
-            float(live["buy_volume"]) > avg_vol and
-            float(live["buy_stddev"]) < std_dev * 1.5
+            float(buy_avg * (1.0 + buy_fee)) < avg_price - std_dev
+            and spread > avg_spread
+            and float(live["buy_volume"]) > avg_vol
+            and float(live["buy_stddev"]) < std_dev * 1.5
         )
 
         sell_flag = (
-            float(sell_avg * (1.0 - sell_fee - sales_tax)) > avg_price + std_dev and
-            spread > avg_spread and
-            float(live["sell_volume"]) > avg_vol and
-            float(live["sell_stddev"]) < std_dev * 1.5
+            float(sell_avg * (1.0 - sell_fee - sales_tax)) > avg_price + std_dev
+            and spread > avg_spread
+            and float(live["sell_volume"]) > avg_vol
+            and float(live["sell_stddev"]) < std_dev * 1.5
         )
 
         if buy_flag and sell_flag:
             continue
 
         if buy_flag:
-            buy.append(live['typeid'])
+            buy.append(live["typeid"])
 
         if sell_flag:
-            sell.append(live['typeid'])
+            sell.append(live["typeid"])
 
     return buy, sell
 
 
-
 def output_order_sheet(buy, sell):
-    print("Buy")
+    print("=========BUY OPPORTUNITIES=========")
     for i in buy:
         print(f"{lookup_type_id(i)}")
-    print("Sell")
+    print("=========SELL OPPORTUNITIES=========")
     for i in sell:
         print(f"{lookup_type_id(i)}")
