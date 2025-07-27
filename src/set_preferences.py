e43354f718a2bb1689866fc99cf46d2586e772d9
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-27 16:47:27 +0900

    added error checking to set_preferences

--- src/set_preferences.py
+++ src/set_preferences.py
@@ -7,20 +7,51 @@ station_to_region = {
 }
 
 def input_percentage(prompt):
-    percent = input(prompt).strip().replace("%", "")
-    return float(percent) / 100
+    res = None
+    while res == None:
+        percent = input(prompt).strip().replace("%", "")
+        try:
+            return float(percent) /100
+        except:
+            pass
 
 def setup_preferences():
-    print("Choose a station:")
-    for i, name in enumerate(station_to_region):
-        print(f"{i+1}. {name}")
-    choice = int(input("Enter choice number: "))
+    choice = None
+    while choice == None:
+        print("Choose a station:")
+        for i, name in enumerate(station_to_region):
+            print(f"{i+1}. {name}")
+        tmp = int(input("enter choice number: "))
+        if tmp in range(1, len(station_to_region) + 1):
+            choice = tmp
     station_name = list(station_to_region.keys())[choice - 1]
     station_info = station_to_region[station_name]
 
-    time = input("Time period (week/month/quarter/year): ").strip().lower()
-    market_size = input("Market size (ISK moved): ").strip()
-    market_volume = input("Market volume (items moved): ").strip()
+    time_options = ["week", "month", "quarter", "year"]
+    time = None
+    while time == None:
+        tmp = input("Time period (week/month/quarter/year): ").strip().lower()
+        if tmp in time_options:
+            time = tmp
+
+    market_size = None
+    while market_size == None:
+        tmp = input("Market size (ISK moved): ").strip()
+        try:
+            int(tmp)
+            market_size = tmp
+        except:
+            continue
+
+    market_volume = None
+    while market_volume == None:
+        tmp = input("Market volume (items moved): ").strip()
+        try:
+            int(tmp)
+            market_volume = tmp
+        except:
+            continue
+
     sales_tax = input_percentage("Sales tax (e.g. 3.5%): ")
     buy_broker_fee = input_percentage("Buy broker fee (e.g. 2.5%): ")
     sell_broker_fee = input_percentage("Sell broker fee (e.g. 3.0%): ")
