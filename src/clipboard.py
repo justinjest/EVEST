2a12bdedb8ad1c7eb5ca9348113f91f17f7d8939
Maximilian Hart <38765373+MaximilianHart@users.noreply.github.com>
2025-07-27 22:25:40 -0500

    fix: adjusted function name in clipboard

--- src/clipboard.py
+++ src/clipboard.py
@@ -1,7 +1,7 @@
 import pyperclip
 
 
-def array_to_clipboard(array):
+def to_clipboard(array):
     string = "\n".join(map(str, array))
     pyperclip.copy(string)
     print("Copied to clipboard:", pyperclip.paste())
